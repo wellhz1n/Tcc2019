b830f7512fc324ca6c2f5118388e0a40ec51c39a
wellington <wellingtom1234520@gmail.com>
2019-04-14 17:47:41 -0300

    mudanca no layout

--- public/pages/contato.php
+++ public/pages/contato.php
@@ -1,26 +1,29 @@
-<h2>Contato</h2>
-    <div class="row">
-        <div class="col-6 offset-3   text-center alert-dark ">
 
-            <h3 class="text-primary"> Contato </h3>
-            <form action="pages/forms/contato.php" method="POST">
-            <input type="text" name="nome" placeholder="Seu Nome" classs='form-control' />
+<div class="row mt-5">
+    <div class="col-6 offset-3   font-Montserrat text-center alert-dark   ">
+
+            <h3 class="text-primary text-center font-weight-bold"> Contato </h3>
+            <form action="pages/forms/contato.php" method="POST" class="m-0">
+                <div class="m-0 form-group">
+
+                    <input type="text" name="nome" placeholder="Seu Nome" class='form-control' />
+                </div>
             <div class="row mt-2"></div>
-            <input type="email" name="email" placeholder="Seu email" classs='form-control' />
+            <input type="email" name="email" placeholder="Seu email" class='form-control' />
             <div class="row mt-2"></div>
-            <input type="text" name="asunto" placeholder="asunto" classs='form-control' />
+            <input type="text" name="asunto" placeholder="asunto" class='form-control' />
             <div class="row mt-2"></div>
             <textarea name="message" rows="10" cols="20" class="form-control" placeholder="Sua Sugestão"></textarea>
             <div class="row mt-2 offset-5 text-center">
-              <button type="submit" class="btn btn-primary">Enviar</button>
-
+                
             </div>
-
-
-
-
-            </form>
-
+            
+            
+            
+            
+        </form>
+        
+        <button type="submit" class="btn btn-primary m-3">Enviar</button>
 
         </div>
     </div>
