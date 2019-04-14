19f436f1467f51d551e524ddbd659f5a16eed74c
wellington <wellingtom1234520@gmail.com>
2019-04-14 12:49:48 -0300

    err

--- public/pages/catalogo.php
+++ public/pages/catalogo.php
@@ -6,7 +6,7 @@
         
         <div class='bt'>
             <img src=''../img/img1.jpg' alt=''>
-                <p>Video</p>
+                <p>Videos</p>
                 <p>Teste alpha</p>
 
 
