8b8eedc4a479a086046185c285eb0e3fb2ceadf4
wellington <wellingtom1234520@gmail.com>
2019-04-14 12:54:49 -0300

    omega

--- public/pages/catalogo.php
+++ public/pages/catalogo.php
@@ -7,7 +7,7 @@
         <div class='bt'>
             <img src=''../img/img1.jpg' alt=''>
                 <p>Videos</p>
-                <p>Teste alpha</p>
+                <p>Teste Omega</p>
 
 
 
