054041b1b174f6607202e49c6190d489327bc15d
wellhz1n <48489005+wellhz1n@users.noreply.github.com>
2019-04-17 20:43:11 -0300

    Validacao da pagina de contato

--- public/assets/js/site/modal.js
+++ public/assets/js/site/modal.js
@@ -1,38 +1,37 @@
-window.onload= function (){
+window.onload = function () {
 
 
 
 
-    function iniciaModal(modalID){
+    function iniciaModal(modalID) {
         const modal = document.getElementById(modalID);
-        if(modal){
+        if (modal) {
 
             modal.classList.add('ativa');
             console.log(modalID);
-            modal.addEventListener('click', (e)=>{
-            if(e.target.id == modalID  || e.target.className =='fechar')
-            {  
-                modal.classList.remove('ativa');
-            }
-           
+            modal.addEventListener('click', (e) => {
+                if (e.target.id == modalID || e.target.className == 'fechar') {
+                    modal.classList.remove('ativa');
+                }
+
 
             });
-         
+
 
         }
 
     }
     const about = document.querySelector('.about');
-     about.addEventListener('click', ()=>iniciaModal('modal'));
+    about.addEventListener('click', () => iniciaModal('modal'));
 
-     $(function(){
+    $(function () {
         // this will get the full URL at the address bar
         var url = window.location.href;
         console.log(url);
         // passes on every "a" tag
-        $("#collapsibleNavbar a").each(function() {
-                // checks if its the same on the address bar
-            if(url == (this.href)) {
+        $("#collapsibleNavbar a").each(function () {
+            // checks if its the same on the address bar
+            if (url == (this.href)) {
                 $(this).closest("li").addClass("ativo");
             }
         });
@@ -40,60 +39,86 @@ window.onload= function (){
 
 
 };
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $(".arrow").click(function(){
-
-
+    $(".arrow").click(function () {
 
 
 
-        $('html, body').animate({scrollTop: $('#target').offset().top +8}, 'medium');
+
+
+        $('html, body').animate({ scrollTop: $('#target').offset().top + 8 }, 'medium');
 
 
     });
 
 
-    $("#nome").focusout(()=>{
 
 
-  var txt = $("#nome").val();
-  
-      txt==""?$("#nome").addClass("border-danger"):$("#nome").removeClass("border-danger");
-        
-        });
-
-        $("#email").focusout(()=>{
 
 
-            var txt = $("#email").val();
-            
-                txt==""?$("#email").addClass("border-danger"):$("#email").removeClass("border-danger");
-                  
-                  });
-                  $("#assunto").focusout(()=>{
+
+    Validacao();
 
 
-                    var txt = $("#assunto").val();
-                    
-                        txt==""?$("#assunto").addClass("border-danger"):$("#assunto").removeClass("border-danger");
-                          
-                          });
-                          $("#mensage").focusout(()=>{
+   
 
-
-                            var txt = $("#mensage").val();
-                            
-                                txt==""?$("#mensage").addClass("border-danger"):$("#mensage").removeClass("border-danger");
-                                  
-                                  });
-                          
-                  
-          
 
 
 
 });
+////Funcao Para Validacao dos Campos Qunado Possivel Separar em Outro Arquivo/////
+function Validacao() {
+    
+$("#span1").hide();
+$("#span2").hide();
+$("#span3").hide();
+$("#span4").hide();
+    $("#nome").focusout(() => {
+        var txt = $("#nome").val();
+        if (txt == "") {
+            $("#nome").addClass("border-danger");
+            $("#span1").fadeIn(200);
+        }
+        else {
+            $("#nome").removeClass("border-danger");
+            $("#span1").fadeOut(200);
+        }
+    });
+    $("#email").focusout(() => {
+        var txt = $("#email").val();
+        if (txt == "") {
+            $("#email").addClass("border-danger");
+            $("#span2").fadeIn(200);
+        }
+        else {
+            $("#email").removeClass("border-danger");
+            $("#span2").fadeOut(200);
+        }
+    });
+    $("#assunto").focusout(() => {
+        var txt = $("#assunto").val();
+        if (txt == "") {
+            $("#assunto").addClass("border-danger");
+            $("#span3").fadeIn(200);
+        }
+        else {
+            $("#assunto").removeClass("border-danger");
+            $("#span3").fadeOut(200);
+        }
+    });
+    $("#mensage").focusout(() => {
+        var txt = $("#mensage").val();
+        if (txt == "") {
+            $("#mensage").addClass("border-danger");
+            $("#span4").fadeIn(200);
+        }
+        else {
+            $("#mensage").removeClass("border-danger");
+            $("#span4").fadeOut(200);
+        }
+    })();
+}
 //
 // $(window).scroll(function(){
 //
--- public/pages/contato.php
+++ public/pages/contato.php
@@ -6,14 +6,18 @@
             <form action="pages/forms/contato.php" method="POST" class="m-0">
                 <div class="m-0 form-group">
 
-                    <input type="text" id="nome" name="nome" placeholder="Seu Nome" class='form-control '  />
+                    <input type="text" id="nome" name="nome" placeholder="Seu Nome" class='form-control '/>
+                    <span class="text-danger"  id="span1">Campo Nescessário</span>
                 </div>
             <div class="row mt-2"></div>
             <input type="email" name="email" id="email" placeholder="Seu email" class='form-control' />
+            <span class="text-danger" id="span2">Campo Nescessário</span>
             <div class="row mt-2"></div>
             <input type="text" name="asunto" id="assunto" placeholder="Assunto" class='form-control' />
+            <span class="text-danger" id="span3">Campo Nescessário</span>
             <div class="row mt-2"></div>
             <textarea name="message" rows="5" cols="20" id="mensage"  class="form-control " placeholder="Sua Sugestão"></textarea>
+            <span class="text-danger" id="span4">Campo Nescessário</span>
             <div class="row mt-2 offset-5 text-center">
                 
             </div>
@@ -23,7 +27,7 @@
             
         </form>
         
-        <button type="submit" onclick="alert('Enviado!')" class="btn btn-primary m-3">Enviar</button>
+        <button type="submit" id="Enviar" class="btn btn-primary m-3">Enviar</button>
 
         </div>
     </div>
