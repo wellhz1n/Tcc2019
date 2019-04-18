f1f51e5beec00c2b9d19c3fbe5a93fb48274f1b7
wellington <wellingtom1234520@gmail.com>
2019-04-17 21:51:15 -0300

    ALinhamento DO Catalogo com Bootstrap

--- public/pages/catalogo.php
+++ public/pages/catalogo.php
@@ -1,11 +1,10 @@
-<section class="caixas">
+<section class="text-center col-12">
     <?php
-    for ($i=0; $i < 5 ; $i++) {
+    for ($i=0; $i < 8 ; $i++) {
         echo  "<div class='box-complete '>
 
                 <div class='bt font-Arimo'>
-                  <div style='padding:100% 0 0 0;position:relative;'><iframe src='https://player.vimeo.com/video/311008428?autoplay=1&loop=1&title=0&byline=0&portrait=0' style='position:absolute;top:0;left:0;width:100%;height:60%;' frameborder='0' allow='autoplay; fullscreen' allowfullscreen></iframe></div><script src='https://player.vimeo.com/api/player.js'></script>
-
+                
 
 
 
