e10bdf73c63a195702b4a6d8b7c114929d4d4700
wellington <wellingtom1234520@gmail.com>
2019-04-18 19:32:14 -0300

    bug Resoolvido

--- public/pages/catalogo.php
+++ public/pages/catalogo.php
@@ -4,7 +4,8 @@
         echo  "<div class='box-complete '>
 
                 <div class='bt font-Arimo'>
-                  <div style='padding:100% 0 0 0;position:relative;'><a href='#'></div>";//<iframe src='https://player.vimeo.com/video/311008428?autoplay=1&loop=1&title=0&byline=0&portrait=0' style='position:absolute;top:0;left:0;width:100%;height:60%;' frameborder='0' allow='autoplay; fullscreen' allowfullscreen></iframe></div><script src='https://player.vimeo.com/api/player.js'></script>
+                
+                
 
 
 
