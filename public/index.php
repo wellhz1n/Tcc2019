5ac642049c8284d220c19b8ee1031d1d7a8753fc
wellington <wellingtom1234520@gmail.com>
2019-04-18 20:53:35 -0300

    Footer

--- public/index.php
+++ public/index.php
@@ -10,7 +10,10 @@
           <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
           <link rel="stylesheet" href="assets/css/site/estilo.css">
           <link rel="stylesheet" href="assets/css/site/fonts.css">
-	      <link rel="stylesheet" href="assets/css/site/paralax.css">
+          <link rel="stylesheet" href="assets/css/site/paralax.css">
+          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+          <script defer src="https://use.fontawesome.com/releases/v5.8.1/js/all.js" integrity="sha384-g5uSoOSBd7KkhAMlnQILrecXvzst9TdC09/VM+pjDTCM+1il8RHz5fKANTFFb+gQ" crossorigin="anonymous"></script>
+
 
           <link rel="stylesheet" href="assets/css/bootstrap/bootstrap.css">
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -37,7 +40,8 @@
 
     <?php  require load();
     ?>
-
+<?php  require 'pages/footer.php' ?>
 <?php require "pages/modal.php" ?>
+
 </body>
 </html>
