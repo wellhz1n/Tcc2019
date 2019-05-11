8c6b68d1509c552e54692acf40022b4f906285a4
wellington <wellingtom1234520@gmail.com>
2019-05-10 21:53:26 -0300

    Novo DB

--- DB/db1/Tcc.sql
+++ DB/db1/Tcc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: 09-Maio-2019 às 17:07
+-- Generation Time: 11-Maio-2019 às 02:52
 -- Versão do servidor: 10.1.38-MariaDB
 -- versão do PHP: 7.3.2
 
@@ -42,7 +42,8 @@ CREATE TABLE `contato` (
 
 INSERT INTO `contato` (`id`, `Nome`, `Email`, `Assunto`, `data_envio`) VALUES
 (23, 'Wellington Hellstrom', 'wellingtom1234520@gmail.com', 'dad', '2019-05-06 12:27:00'),
-(24, 'hj', 'wellingtom1234520@gmail.com', 'hknjk', '2019-05-07 10:20:00');
+(24, 'hj', 'wellingtom1234520@gmail.com', 'hknjk', '2019-05-07 10:20:00'),
+(25, 'vbhjv ', 'wellingtom1234520@gmail.com', 'vjvmv', '2019-05-10 14:22:00');
 
 -- --------------------------------------------------------
 
@@ -63,11 +64,13 @@ CREATE TABLE `produto` (
 --
 
 INSERT INTO `produto` (`id`, `nome`, `descricao`, `img`, `valor`) VALUES
-(4, 'sdads', NULL, 'c2eb0c7658bc1b5f6c04ae4e85eda6a0.jpeg', 43343),
-(5, 'sss', NULL, '4152cccee6675b98c9607bb00fe339cf.png', 23232),
-(6, 'passaro', NULL, 'c0020a034306d486982ae1c3ab6ab70f.gif', 54534),
-(7, 'mkrwkr', NULL, 'd66b64987eb72f90e97a3fc7461499da.gif', 424242),
-(8, 'html', NULL, 'adca0832202a5670ce6f81a1edba3004.png', 33323);
+(12, 'C#', NULL, '1cd5e6423b158615eec037c41cf3dd20.png', 200),
+(14, 'VUE JS', 'Uma Ferramenta JavaScript poderos para geraÃ§Ã£o de Interfaces Graficas.', '86eeb9ea2c0b6a31bd4ef261e8152f65.png', 20),
+(15, 'Aula HTML', 'Uma Linguagem Basica para desenvolvimento WEB.', '10a2d60388a3d6d693309858f3208666.png', 200),
+(16, 'JavaScript', 'Uma Linguagem Melhor que Java, NÃ£o Ã© difÃ­cil o mateus ta de mimimi!', '2c5ec69350240bf98fa93b48f913ab40.png', 1000),
+(17, 'Corpo Humano', 'Unico no estoque', '7a788c4124db4d124101d319ad11d871.png', 400000000),
+(19, 'html', 'html', '828672bb52f3a577a4ce86e8cd26a8c5.png', 1),
+(20, 'passaro', 'n tem', '9f59fec4c5665b8a25dea3542263114b.gif', 88978979);
 
 -- --------------------------------------------------------
 
@@ -79,19 +82,20 @@ CREATE TABLE `usuario` (
   `id_user` int(11) NOT NULL,
   `nome` varchar(255) NOT NULL,
   `senha` varchar(255) NOT NULL,
-  `nivel_autoridade` tinyint(1) NOT NULL
+  `nivel_autoridade` tinyint(1) NOT NULL,
+  `img` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `usuario`
 --
 
-INSERT INTO `usuario` (`id_user`, `nome`, `senha`, `nivel_autoridade`) VALUES
-(53, 'admin     ', '21232f297a57a5a743894a0e4a801fc3', 0),
-(54, 'wellington', 'dcbacadf485c141a2b9b0028f2c0b2e1', 1),
-(68, 'root', '63a9f0ea7bb98050796b649e85481845', 0),
-(69, 'mateus  ', 'dcbacadf485c141a2b9b0028f2c0b2e1', 1),
-(70, 'Igor  ', 'dd97813dd40be87559aaefed642c3fbb', 0);
+INSERT INTO `usuario` (`id_user`, `nome`, `senha`, `nivel_autoridade`, `img`) VALUES
+(53, 'admin     ', '21232f297a57a5a743894a0e4a801fc3', 0, NULL),
+(54, 'wellington ', 'dcbacadf485c141a2b9b0028f2c0b2e1', 0, NULL),
+(68, 'root', '63a9f0ea7bb98050796b649e85481845', 0, NULL),
+(69, 'mateus  ', 'dcbacadf485c141a2b9b0028f2c0b2e1', 1, NULL),
+(70, 'Igor    ', 'dd97813dd40be87559aaefed642c3fbb', 1, NULL);
 
 --
 -- Indexes for dumped tables
@@ -107,8 +111,7 @@ ALTER TABLE `contato`
 -- Indexes for table `produto`
 --
 ALTER TABLE `produto`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `nome` (`nome`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `usuario`
@@ -125,13 +128,13 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT for table `contato`
 --
 ALTER TABLE `contato`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `produto`
 --
 ALTER TABLE `produto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `usuario`
