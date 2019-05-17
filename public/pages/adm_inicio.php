a76d08d2c4586d7d4b795e89693ac3c428d8ae19
wellington <wellingtom1234520@gmail.com>
2019-05-16 21:59:13 -0300

    Foto Login

--- public/pages/adm_inicio.php
+++ public/pages/adm_inicio.php
@@ -6,9 +6,13 @@ if( isset($_SESSION['login'], $_SESSION['id']) ){
 ?>
 
 <div style="background:gray;" class=" cont text-center ">
+<?php if($usuario['img'] != ""){ ?> 
+        <img  class ="rounded-circle" src="assets/img/usuario/<?php echo $usuario['img'];  ?>" >
+<?php } else{?>
     <div class="ft"></div>
+<?php } ?>
     <h1>Bem Vindo <label class="text-danger"><?php echo $usuario['nome']; ?></label> </h1>
-    
+    <button type="button" onclick="window.location.href ='http://localhost:8081/Tcc/public/?page=usuario_foto ' "  href="?page=usuario_foto" class='btn btn-primary '>Mudar Imagem de perfil</button>
     
     
     
