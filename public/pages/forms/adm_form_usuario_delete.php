0508136a91d73a3f9c6fc5205e220fa1eeb0da3e
Nidilap <nidilap11@hotmail.com>
2019-06-07 20:02:48 -0300

    la

--- public/pages/forms/adm_form_usuario_delete.php
+++ public/pages/forms/adm_form_usuario_delete.php
@@ -1,12 +1,11 @@
 <?php
 
- require '../../../bootstrap.php';
+require '../../../bootstrap.php';
 require "../../../app/function/DB.php";
- $id = $_POST['id'];
-
-echo $id;
-$deleteUser = mysqli_query($cx,"DELETE FROM usuario  WHERE usuario.id_user = $id");
-// 
+$id = $_POST['id'];
+echo dd($id);
+$deleteUser = mysqli_query($cx,"DELETE FROM usuario WHERE usuario.id_user = $id");
+//
 // header("Location: http://localhost:8081/Tcc/public/?page=adm_usuario");
 // die();
 //
